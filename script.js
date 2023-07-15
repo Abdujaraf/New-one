81e6f2ed8253e2bd3d24b8553e8b51e660c9c142
Abdujaraf <mkarshibaev12@gmail.com>
2023-07-15 16:44:15 +0500

    new creation

---
+++ script.js
@@ -0,0 +1,106 @@
+// let number = 12
+// const number2 = '13'
+// alert(number2)
+// let name = prompt('Enter your name')
+// alert(name)
+// let number = +prompt('Enter the number')
+// if (number > 0) {
+//     alert('Positive number')
+// } else if(number == 0) {
+//     alert('This is null')
+// }else {
+//     alert('Negative number')
+// }
+
+
+// let first_number = +prompt('Enter the first number')
+// let second_number = +prompt('Enter the second number')
+// let operation = prompt('Enter the operation')
+// if(operation == '+') {
+//     let result = first_number + second_number
+//     alert(result)
+// } else if (operation == '-') {
+//     let result = first_number - second_number
+//     alert(result)
+// } else if (operation == '*') {
+//     let result = first_number * second_number
+//     alert(result)
+// } else if (operation == '/') {
+//     let result = first_number / second_number
+//     alert(result)
+// } else {
+//     alert('Error')
+// }
+
+
+// let first_number = +prompt('Enter the first number')
+// let second_number = +prompt('Enter the second number')
+// let operation = prompt('Enter the operation')
+// switch(operation) {
+//     case '+':
+//         result = first_number + second_number
+//         break
+//     case '-':
+//         result = first_number - second_number
+//         break  
+//     case '*':
+//         result = first_number + second_number
+//         break 
+//     case '/':
+//         result = first_number / second_number
+//         break  
+//     default:
+//         alert('Error')
+// }
+// alert(result)
+
+// let number = 12
+// let numbers = [12,23,45,'ytu',true,67,744,75,77,85]
+// // alert(numbers[1])
+// // alert(numbers.length)
+// // alert(numbers[numbers.length - 1])
+// numbers.push(34)
+// alert(numbers)
+// numbers.pop()
+// alert(numbers)
+
+// let group = ['Abdumalik','Sardor','Farux','Vsevolod']
+// let student_name = prompt('Enter the student name')
+// group.push(student_name)
+// alert(group)
+// alert(group[group.length - 1])
+
+// for(i = 0;i < 10;i++){
+//     alert('Hello World')
+// }
+
+
+// let object = {
+//     name:'Abdumalik',
+//     age:20,
+// }
+
+
+// let group = []
+// let student_number = +prompt('Enter the student number')
+// for(i=0;i<student_number;i++) {
+//     let student_name = prompt('Enter the student name')
+//     let student_surname = prompt('Enter the student surname')
+//     group.push({
+//         name:student_name,
+//         surname:student_surname
+//     })
+// }
+// console.log(group)
+
+function sum(a,b) {
+    let result = a + b
+    return result
+}
+
+
+alert(sum(10,12))
+
+
+
+
